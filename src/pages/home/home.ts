879638e9e7d0a22b6db014b9fa5d4e41f65dddda
Daljeet Singh <daljeet19881119@gmail.com>
2018-05-10 17:31:25 +0530

    New HeatApp Design is completed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController } from 'ionic-angular';
+import { ProfilePage } from '../profile/profile';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  // icons
+  icons: string = 'camera';
 
+  // latestDonations
+  latestDonations: any = [
+    {img: 'assets/imgs/founder_Juccce.jpg', name: 'Juccce', date: '5/05/2018', heartCoin: 100, usd: '25.00', total: '10,048.39'},
+    {img: 'assets/imgs/founder_OpenCity.jpg', name: 'Open City', date: '5/05/2018', heartCoin: 300, usd: '75.00', total: '10,048.39'},
+    {img: 'assets/imgs/founder_ReviveCongo.jpg', name: 'Revive Congo', date: '5/05/2018', heartCoin: 10, usd: '25.00', total: '10,048.39'},
+    {img: 'assets/imgs/founder_SaveTheChildren.png', name: 'Save The Children', date: '5/05/2018', heartCoin: 300, usd: '75.00', total: '10,048.39'}
+  ];
+
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
+    
   }
 
+  // showTabs
+  showTabs() {
+    console.log('you selected: '+this.icons);
+  }
+
+  // gotoProfile
+  gotoProfile() {
+    this.navCtrl.push(ProfilePage);
+  }
 }
